620f5971b0e5fe6f0203e1e5d459e1dd9a722f49
afrings <afrings@wisc.edu>
2023-10-03 17:06:38 -0500

    added dynamodb to backend

---
+++ back/src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-2'})
+const client = new AWS.DynamoDB.DocumentClient();
+const tableName = 'MailSources';
+
+const port = 3000;
+
+app.get("/rows/all", (req, res) => {
+    var params = {
+        TableName: tableName
+    };
+    
+    client.scan(params, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            var items = [];
+            for (var i in data.Items)
+                items.push(data.Items[i]);
+            res.contentType = 'application/json'
+            res.send(items);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
