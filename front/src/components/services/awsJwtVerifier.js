605201113637dd9296b0ec4e066fdc8c16f813d9
afrings <afrings@wisc.edu>
2023-11-28 10:15:59 -0600

    changed userpool

--- front/src/components/services/awsJwtVerifier.js
+++ front/src/components/services/awsJwtVerifier.js
@@ -3,7 +3,7 @@ import { CognitoJwtVerifier } from 'aws-jwt-verify';
 const Verifier = CognitoJwtVerifier.create({
     userPoolId: process.env.REACT_APP_DASHBOARD_USER_POOL_ID,
     clientId: process.env.REACT_APP_DASHBOARD_CLIENT_ID,
-    tokenUse:'id',
+    tokenUse:'access',
 })
 
 export default Verifier;
--- front/src/components/services/userpool.js
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js'
-
-const Userpool = new CognitoUserPool({
-    UserPoolId: process.env.REACT_APP_DASHBOARD_USER_POOL_ID,
-    ClientId: process.env.REACT_APP_DASHBOARD_CLIENT_ID,
-});
-
-export default Userpool;
