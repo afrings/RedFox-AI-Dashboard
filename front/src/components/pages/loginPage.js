f65ec609ce106476ec3c7ea7b357d1f938b2cdac
afrings <afrings@wisc.edu>
2023-12-08 14:15:35 -0600

    login flow in working order

--- front/src/components/pages/loginPage.js
+++ front/src/components/pages/loginPage.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { signIn, signOut, confirmSignIn, fetchAuthSession, setUpTOTP } from 'aws-amplify/auth';
+import { signIn, signOut, confirmSignIn, fetchAuthSession } from 'aws-amplify/auth';
+import { Box, Card, CardContent, CardMedia, Grid } from '@mui/material';
 import QRCode from 'react-qr-code';
 // import ReCAPTCHA from 'react-google-recaptcha';
 import isPasswordValid from '../services/isPasswordValid';
+import imageUrl from '../assests/Secondary-CMYK.png';
 
 export default function Login() {
 
@@ -13,22 +15,28 @@ export default function Login() {
     const [passwordWarning, setPasswordWarning] = useState({passwordWarningMessage: ''});
     const [mfa, setMfa] = useState({mfaCode: '', mfaUri: ''});
 
-    var totpPromise;
-
-    // useEffect(() => {
-    //     var button = document.getElementById('button');
-    //     console.log(button);
-    //     if (button) {
-    //         console.log('createButton')
-    //         totpPromise = new Promise((resolve) => {
-    //             button.addEventListener('click', function() {
-    //                 console.log('click')
-    //                 console.log(mfa.mfaCode);
-    //                 resolve(mfa.mfaCode);
-    //             });
-    //         });
-    //     }
-    // });
+    // custom event and promise to pass totp to the login function
+    const event = new Event('totp');
+    var totpPromise = new Promise((resolve) => {
+        document.addEventListener('totp', (e) => {
+            resolve(document.getElementById('mfaInput').value);
+        });
+    });
+    
+    // give value to the button once the correct part of the login stage has been reached
+    var button;
+    useEffect(() => {
+        if (loginStage === 'mfaSetup' || loginStage === 'mfa'){
+            button = document.getElementById('button');
+            console.log(button);
+            if (button) {
+                // button click event triggers custom event declared above
+                button.addEventListener('click', function() {
+                    document.dispatchEvent(event);
+                });
+            }
+        }
+    });
 
     const handleChangeMfaCode = (event) => {
         setMfa({mfaCode: event.target.value, mfaUri: mfa.mfaUri});
@@ -71,31 +79,76 @@ export default function Login() {
         try{
             console.log(`Username: ${username} Password: ${savedPassword.useSavedPassword ? savedPassword.savedPassword : password}`);
             const { isSignedIn, nextStep } = await signIn({ username: username, password: savedPassword.useSavedPassword ? savedPassword.savedPassword : password });
-            const { accessToken, idToken } = (await fetchAuthSession()).tokens ?? {};
+            var { accessToken, idToken } = (await fetchAuthSession()).tokens ?? {};
             console.log(nextStep)
             switch (nextStep.signInStep) {
                 case 'CONFIRM_SIGN_IN_WITH_NEW_PASSWORD_REQUIRED':
+                    console.log('request new password');
                     // store the temporary password
                     setSavedPassword({useSavedPassword: true, savedPassword: password});
-                    setNewPassword();
+                    setLoginStage('requestNewPassword');
+                    setState({username: state.username, password: '', newPassword: state.newPassword});
+                    if (isPasswordValid(state.newPassword) && isPasswordValid(state.password) && state.newPassword === state.password){
+                        try {
+                            var response = await confirmSignIn({challengeResponse: state.password});
+                            console.log(response);
+                            setState({username: state.username, password: '', newPassword: ''});
+                            setSavedPassword({useSavedPassword: false, savedPassword: ''});
+                            setLoginStage('login');
+                        } catch(e) {
+                            console.log(e);
+                        }
+                    }
+                    break;
 
                 case 'CONTINUE_SIGN_IN_WITH_TOTP_SETUP':
+                    console.log('mfaSetup');
                     const totpSetupDetails = nextStep.totpSetupDetails;
                     const appName = 'RedFox AI Dashboard';
                     const setupUri = totpSetupDetails.getSetupUri(appName);
                     setMfa({mfaUri: setupUri.href, mfaCode: mfa.mfaCode});
 
+                    setLoginStage('mfaSetup');
+
+                    var totp = await totpPromise;
+                    console.log('TOTP RESPONSE: ', totp);
+                    var needTotp = true;
+                    while (needTotp) {
+                        try {
+                            var response = await confirmSignIn({challengeResponse: totp});
+                            needTotp = false;
+                        } catch (e) {
+                            totpPromise = new Promise((resolve) => {
+                                document.addEventListener('totp', (e) => {
+                                    resolve(document.getElementById('mfaInput').value);
+                                });
+                            });
+                            totp = await totpPromise;
+                        }
+                    }
+                    var { accessToken, idToken } = (await fetchAuthSession()).tokens ?? {};
+                    break;
+
+                case 'CONFIRM_SIGN_IN_WITH_TOTP_CODE':
                     setLoginStage('mfa');
-                    console.log('here');
-                    var button = document.getElementById('button');
-                    button.addEventListener('click', async function() {
-                        console.log('click');
-                        const totp = await totpPromise;
-                        console.log(totp);
-                    })
-                    
-                    // const response = await confirmSignIn({challengeResponse: totp});
-                    // console.log(response);
+                    var totp = await totpPromise;
+                    console.log('TOTP RESPONSE: ', totp);
+                    var needTotp = true;
+                    while (needTotp) {
+                        try {
+                            var response = await confirmSignIn({challengeResponse: totp});
+                            needTotp = false;
+                        } catch (e) {
+                            totpPromise = new Promise((resolve) => {
+                                document.addEventListener('totp', (e) => {
+                                    resolve(document.getElementById('mfaInput').value);
+                                });
+                            });
+                            totp = await totpPromise;
+                        }
+                    }
+                    var { accessToken, idToken } = (await fetchAuthSession()).tokens ?? {};
+                    break;
             }
             if (accessToken) {
                 sessionStorage.setItem('jwt', accessToken.toString());
@@ -116,110 +169,103 @@ export default function Login() {
         }
         
     };
-
-    const setNewPassword = async() => {
-        setLoginStage('requestNewPassword');
-        setState({username: state.username, password: '', newPassword: state.newPassword});
-        if (isPasswordValid(state.newPassword) && isPasswordValid(state.password) && state.newPassword === state.password){
-            try {
-                await confirmSignIn({challengeResponse: state.password});
-                setState({username: state.username, password: '', newPassword: ''});
-                setSavedPassword({useSavedPassword: false, savedPassword: ''});
-                setLoginStage('login');
-            } catch(e) {
-                console.log(e);
-            }
-        }
-    }
     
     return (
-        <div>
-            {loginStage === 'login' ? 
-            // normal login
-            <div style={pageFormat}>
-                <input placeholder={"Username"} value={state.username} onChange={handleChangeUsername} style={inputUsernameStyle}></input>
-                <input placeholder={"Password"} value={state.password} onChange={handleChangePassword} /*type='password'*/ style={inputPasswordStyle}></input>
-                <button style={loginButtonStyle} onClick={(e) => login(state.username, state.password, state.newPassword)}> Login </button>
-            </div>
-            : null}
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+        >
+        <Card raised sx={{border: 1 }}>
+            <CardMedia
+                component='img'
+                sx={{height: 55, objectFit: 'contain', paddingRight: 5, paddingTop: 2}}
+                image={imageUrl}
+                title='redfox'
+            />
+            <CardContent>
+                {loginStage === 'login' ? 
+                // normal login
+                <div style={pageFormat}>
+                    <input placeholder={"Username"} value={state.username} onChange={handleChangeUsername} style={inputUsernameStyle}></input>
+                    <input placeholder={"Password"} value={state.password} onChange={handleChangePassword} /*type='password'*/ style={inputPasswordStyle}></input>
+                    <button style={loginButtonStyle} onClick={(e) => login(state.username, state.password, state.newPassword)}> Login </button>
+                </div>
+                : null}
 
-            {loginStage === 'requestNewPassword' ? 
-            // request new password
-            <div style={pageFormat}>
-                <div style={passwordWarningStyle}> {passwordWarning.passwordWarningMessage} </div>
-                <input placeholder={'New Password'} value={state.password} onChange={handleChangePassword} /*type='password'*/ style={inputPasswordStyle}></input>
-                <input placeholder={'Confirm New Password'} value={state.newPassword} onChange={handleChangeNewPassword} type='password' style={inputNewPasswordStyle}></input>
-                <button style={loginButtonStyle} onClick={(e) => login(state.username, state.password, state.newPassword)}> Set New Password </button>
-            </div>
-            : null}
+                {loginStage === 'requestNewPassword' ? 
+                // request new password
+                <div style={pageFormat}>
+                    <div style={passwordWarningStyle}> {passwordWarning.passwordWarningMessage} </div>
+                    <input placeholder={'New Password'} value={state.password} onChange={handleChangePassword} /*type='password'*/ style={inputPasswordStyle}></input>
+                    <input placeholder={'Confirm New Password'} value={state.newPassword} onChange={handleChangeNewPassword} type='password' style={inputNewPasswordStyle}></input>
+                    <button style={loginButtonStyle} onClick={(e) => login(state.username, state.password, state.newPassword)}> Set New Password </button>
+                </div>
+                : null}
 
-            {loginStage === 'mfa' ? 
-            // request totp
-            <div style={pageFormat}>
-                <QRCode value={mfa.mfaUri} style={mfaQrCodeStyle}/>
-                <input placeholder={'Multi-Factor Authentication Code'} value={mfa.mfaCode} onChange={handleChangeMfaCode} style={mfaCodeInputStyle}></input>
-                <button id='button' style={loginButtonStyle} onClick={null}> Confirm MFA </button>
-            </div>
-            : null}
+                {loginStage === 'mfaSetup' ? 
+                // request totp
+                <div style={pageFormat}>
+                    <QRCode value={mfa.mfaUri} style={mfaQrCodeStyle}/>
+                    <input id='mfaInput' autoComplete='off' placeholder={'Multi-Factor Authentication Code'} value={mfa.mfaCode} onChange={handleChangeMfaCode} style={mfaCodeInputStyle}></input>
+                    <button id='button' style={loginButtonStyle} onClick={null}> Confirm MFA </button>
+                </div>
+                : null}
 
-        </div>
+                {loginStage === 'mfa' ? 
+                // request totp
+                <div style={pageFormat}>
+                    <input id='mfaInput' autoComplete='off' placeholder={'Multi-Factor Authentication Code'} value={mfa.mfaCode} onChange={handleChangeMfaCode} style={mfaCodeInputStyle}></input>
+                    <button id='button' style={loginButtonStyle} onClick={null}> Confirm MFA </button>
+                </div>
+                : null}
+            </CardContent>
+        </Card>
+        </Box>
     );
 }
 
 // Styling
 
 const pageFormat = {
-    position: 'absolute',
     display: 'grid',
-    gridAutoColumns: '11vw',
-    gap: '5px',
-    gridAutoRows: '5vh',
 };
 
 const passwordWarningStyle = {
-    gridColumn: '4/7',
-    gridRow: '6',
     textAlign: 'center',
     fontSize: '20px',
 }
 
 const inputUsernameStyle = {
-    gridColumn: '4/7',
-    gridRow: '9',
     textAlign: 'center',
     fontSize: '20px',
+    padding: '5px 5px 5px 5px',
 };
 
 const inputPasswordStyle = {
-    gridColumn: '4/7',
-    gridRow: '10',
     textAlign: 'center',
     fontSize: '20px',
+    padding: '5px 5px 5px 5px',
 };
 
 const inputNewPasswordStyle = {
-    gridColumn: '4/7',
-    gridRow: '11',
     textAlign: 'center',
     fontSize: '20px',
+    padding: '5px 5px 5px 5px',
 };
 
 const loginButtonStyle = {
-    gridColumn: '5/6',
-    gridRow: '12',
-    fontSize: '16px',
+    fontSize: '20px',
 };
 
 const mfaQrCodeStyle = {
-    gridColumn: '5',
-    gridRow: '4',
     width: 'auto',
     height: 'auto',
 };
 
 const mfaCodeInputStyle = {
-    gridColumn: '5/7',
-    gridRow: '11',
     textAlign: 'center',
     fontSize: '20px',
+    padding: '5px 5px 5px 5px',
 };
